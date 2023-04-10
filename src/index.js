23f58b538b2dc0d1101dc07a88851cd32dc27898
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-04-10 22:50:22 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,52 +1,62 @@
 import './css/styles.css'; // стилі
 import debounce from 'lodash.debounce'; // бібліотека lodash.debounce
 import Notiflix from 'notiflix'; // бібліотека Notiflix
-import API from './js/fetchCountries'; // робота з бекендом
-// import countryCardTpl from './js/templates/country-card.hbs'; // розмітка
-// import countryListTpl from './js/templates/country-list.hbs'; // розмітка
+import { fetchCountries } from './js/fetchCountries'; // робота з бекендом
+// import countryCardTpl from './js/templates/country-card.hbs'; // розмітка (не вийшло реалізувати через шаблони 😔)
+// import countryListTpl from './js/templates/country-list.hbs'; // розмітка (не вийшло реалізувати через шаблони 😔)
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
-  searchInput: document.querySelector('#search-box'),
+  searchInput: document.querySelector('input#search-box'),
   cardList: document.querySelector('.country-list'),
   cardContainer: document.querySelector('.country-info'),
 };
 
 refs.searchInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-// !==================
-
-function onInput(evt) {
-  evt.preventDefault();
-
-  const inputText = evt.target.value.trim();
-  //   console.log(inputText);
-
-  //   if (inputText === '') {
-  //     clearMarkup();
-  //     return;
-  //   }
-
-  API.fetchCountries(inputText)
-    .then(data => {
-      //   console.log(data);
-      if (data.length > 10) {
-        Notiflix.Notify.info(
-          'Too many matches found. Please enter a more specific name.'
-        );
-        return;
-      }
-      //   console.log(data);
-      renderMarkup(data);
-    })
-    .catch(onFetchError);
-  // .finally(() => inputText.reset());
+// Функція створення розмітки картки
+function createCardMarkup(data) {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<div class="card">
+        <div class="card-hader">
+          <img src="${flags.svg}" alt="${
+        name.official
+      }"  width="100" height="50">
+          <h2 class="card-title">${name.official}</h2>
+        </div>
+        <div class="card-body">
+          <p class="card-text"><span>Capital:</span> ${capital}</p>
+          <p class="card-text"><span>Population:</span> ${population}</p>
+          <p class="card-text"><span>Languages:</span> ${Object.values(
+            languages
+          )}</p>
+        </div>
+      </div>`
+  );
 }
 
-// !==================
+// Функція створення розмітки списку
+function createListMarkup(data) {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li class="list-item">
+                <img src="${flags.svg}" alt="${name.official}"  width="40" height="30">
+                <p class="card-title">${name.official}</p>
+        </li>`
+    )
+    .join('');
+}
 
+// Функція візуалізації інтерфейсу
 function renderMarkup(data) {
-  if (data.length === 1) {
+  if (data.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  } else if (data.length === 1) {
     clearMarkup(refs.cardList);
     const markup = createCardMarkup(data);
     refs.cardContainer.innerHTML = markup;
@@ -57,56 +67,35 @@ function renderMarkup(data) {
   }
 }
 
-// !==================
-
-function onFetchError(error) {
-  //   clearMarkup();
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-}
-
-// !==================
-
+// Функція очищення інтерфейсу
 function clearMarkup(ref) {
   ref.innerHTML = '';
 }
 
-// !=================
-
-function createCardMarkup(data) {
-  return data.map(
-    ({ name, capital, population, flags, languages }) =>
-      `<div class="card">
-        <div class="card-img-top">
-            <img src="${flags.svg}" alt="${
-        name.official
-      }"  width="200" height="100">
-            <h2 class="card-title">${name.official}</h2>
-        </div>
-        <div class="card-body">
-            <p class="card-text">Capital: ${capital}</p>
-            <p class="card-text">Population: ${population}</p>
-            <p class="card-text">Languages:
-            ${Object.values(languages)}
-            </p>
-        </div>
-     </div>`
-  );
+// Функція відображення помилки
+function onFetchError(error) {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 }
 
-// !==================
+// !===================
+function onInput(evt) {
+  evt.preventDefault();
 
-function createListMarkup(data) {
-  return data
-    .map(
-      ({ name, flags }) =>
-        `<li class="list-group-item">
-            <div>
-                <img src="${flags.svg}" alt="${name.official}"  width="20" height="10">
-                <p class="card-title">${name.official}</p>
-            </div>
-        </li>`
-    )
-    .join('');
+  const inputText = evt.target.value.trim();
+  //   console.log(inputText);
+
+  // Якщо користувач повністю очищає поле пошуку, то HTTP-запит не виконується, а розмітка списку країн або інформації про країну зникає.
+  if (inputText === '') {
+    clearMarkup(refs.cardList);
+    clearMarkup(refs.cardContainer);
+
+    return;
+  }
+
+  // Рендерить інтерфейс
+  fetchCountries(inputText)
+    .then(data => {
+      renderMarkup(data);
+    })
+    .catch(onFetchError);
 }
-
-// !==================
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,13 @@
-const BASE_URL = 'https://restcountries.com/v3.1';
-const fields = 'fields=name,capital,population,flags,languages';
+// Робить HTTP-запит на ресурс name і повертає проміс з масивом країн - результатом запиту.
+// Явно створює помилку для коду 404
+export const fetchCountries = name => {
+  const BASE_URL = 'https://restcountries.com/v3.1/name';
+  const fields = 'fields=name,capital,population,flags,languages';
 
-function fetchCountries(name) {
-  return fetch(`${BASE_URL}/name/${name}?${fields}`).then(response =>
-    response.json().catch(error => console.error(error))
-  );
-}
-
-export default { fetchCountries };
+  return fetch(`${BASE_URL}/${name}?${fields}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json().catch(error => console.error(error));
+  });
+};
